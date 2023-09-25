5cecffeeb2656c8c96c8d9bd0ba187463d91dcc8
asubaruwrxsti <arlind12344@gmailcom>
2023-09-25 21:14:14 +0200

    uwu login uwu uwu uwu

--- api/index.py
+++ api/index.py
@@ -1,22 +1,80 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from uuid import UUID
+from uuid import UUID, uuid4
 from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from fastapi_sessions.backends.implementations import InMemoryBackend
 from fastapi_sessions.session_verifier import SessionVerifier
 from fastapi import HTTPException
 
 app = FastAPI()
 class SessionData(BaseModel):
+    token: UUID
     username: str
     password: str
-    token: UUID
+
+class BasicVerifier(SessionVerifier[UUID, SessionData]):
+    def __init__(
+        self,
+        *,
+        identifier: str,
+        auto_error: bool,
+        backend: InMemoryBackend[UUID, SessionData],
+        auth_http_exception: HTTPException,
+    ):
+        self._identifier = identifier
+        self._auto_error = auto_error
+        self._backend = backend
+        self._auth_http_exception = auth_http_exception
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def backend(self):
+        return self._backend
+
+    @property
+    def auto_error(self):
+        return self._auto_error
+
+    @property
+    def auth_http_exception(self):
+        return self._auth_http_exception
+
+    def verify_session(self, model: SessionData) -> bool:
+        """If the session exists, it is valid"""
+        return True
+
+backend = InMemoryBackend[UUID, SessionData]()
+verifier = BasicVerifier(
+    identifier="general_verifier",
+    auto_error=True,
+    backend=backend,
+    auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
+)
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
 
 @app.post("/api/login")
-def login():
+async def login(response: Response):
+    cookie_params = CookieParameters()
+    cookie = SessionCookie(
+        cookie_name="cookie",
+        identifier="general_verifier",
+        auto_error=True,
+        secret_key="DONOTUSE",
+        cookie_params=cookie_params,
+    )
+
+    session = uuid4()
+    data = SessionData(token=session, username="admin", password="admin")
+
+    await backend.create(session, data)
+    cookie.attach_to_response(response, session)
+
     return {
         "status": True,
         "message": "Login Success",
